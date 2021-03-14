a8633353e943c5747f9d009e1030a7d599e079f3
xtu502 <46643161+xtu502@users.noreply.github.com>
2021-03-14 18:06:44 +0800

    Add files via upload

---
+++ Insect_pest/load_plt.py
@@ -0,0 +1,305 @@
+﻿
+
+from keras.layers import GlobalAveragePooling2D, GlobalMaxPooling2D, Reshape, Dense, multiply, Permute, Concatenate, Conv2D, Add, Activation, Lambda
+from keras import backend as K
+from keras.activations import sigmoid
+
+
+from keras import backend as K
+from keras import optimizers
+from keras.optimizers import Adam
+import keras.backend.tensorflow_backend as KTF
+import glob
+from keras.layers import Input,Dense,Dropout,BatchNormalization,Conv2D,MaxPooling2D,AveragePooling2D,concatenate,Activation,ZeroPadding2D
+import tensorflow as tf
+import cv2
+import numpy as np
+import pandas as pd
+import keras
+from keras.models import load_model
+from keras.layers import Activation, Dense
+from matplotlib import pyplot as plt
+from skimage import io,data
+import time
+from keras import layers
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.preprocessing.image import ImageDataGenerator
+
+from keras.preprocessing import image
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+now = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
+
+from keras.layers import SeparableConv2D,ReLU
+
+import os,sys
+os.getcwd()
+os.chdir("/home/cjd/41_pest_pub")
+
+print(os.getcwd())
+print (sys.version)
+
+
+#import os
+# 
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "3,5"
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+keras.backend.tensorflow_backend.set_session(tf.Session(config=config))
+
+
+
+def replace_intermediate_layer_in_keras(model, layer_id, new_layer):
+    from keras.models import Model
+
+    layers = [l for l in model.layers]
+
+    x = layers[0].output
+    for i in range(1, len(layers)):
+        if i == layer_id:
+            x = new_layer(x)
+        else:
+            x = layers[i](x)
+
+    new_model = Model(input=layers[0].input, output=x)
+    return new_model
+
+def insert_intermediate_layer_in_keras(model, layer_id, new_layer):
+    from keras.models import Model
+
+    layers = [l for l in model.layers]
+
+    x = layers[0].output
+    for i in range(1, len(layers)):
+        if i == layer_id:
+            x = new_layer(x)
+        x = layers[i](x)
+
+    new_model = Model(input=layers[0].input, output=x)
+    return new_model
+
+
+
+
+import tensorflow as tf        
+def focal_loss(gamma=2.):            
+    def focal_loss_fixed(y_true, y_pred):
+        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+        return -K.sum( K.pow(1. - pt_1, gamma) * K.log(pt_1)) 
+    return focal_loss_fixed
+
+
+def Conv2d_BN(x, nb_filter,kernel_size, strides=(1,1), padding='same',name=None):  
+    if name is not None:  
+        bn_name = name + '_bn'  
+        conv_name = name + '_conv'  
+    else:  
+        bn_name = None  
+        conv_name = None  
+  
+    x = Conv2D(nb_filter,kernel_size,padding=padding,strides=strides,activation='relu',name=conv_name)(x)  
+    x = BatchNormalization(axis=3,name=bn_name)(x)  
+    return x  
+
+def Conv_Block(inpt,nb_filter,kernel_size,strides=(1,1), with_conv_shortcut=False):  
+    x = Conv2d_BN(inpt,nb_filter=nb_filter[0],kernel_size=(1,1),strides=strides,padding='same')  
+    x = Conv2d_BN(x, nb_filter=nb_filter[1], kernel_size=(3,3), padding='same')  
+    x = Conv2d_BN(x, nb_filter=nb_filter[2], kernel_size=(1,1), padding='same')  
+    if with_conv_shortcut:  
+        shortcut = Conv2d_BN(inpt,nb_filter=nb_filter[2],strides=strides,kernel_size=kernel_size)  
+        x = add([x,shortcut])  
+        return x  
+    else:  
+        x = add([x,inpt])  
+        return x  
+
+
+def channel_attention(input_feature, ratio=8):
+	
+	channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+	channel = input_feature._keras_shape[channel_axis]
+	
+	shared_layer_one = Dense(channel//ratio,
+							 kernel_initializer='he_normal',
+							 activation = 'relu',
+							 use_bias=True,
+							 bias_initializer='zeros')
+
+	shared_layer_two = Dense(channel,
+							 kernel_initializer='he_normal',
+							 use_bias=True,
+							 bias_initializer='zeros')
+	
+	avg_pool = GlobalAveragePooling2D()(input_feature)    
+	avg_pool = Reshape((1,1,channel))(avg_pool)
+	assert avg_pool._keras_shape[1:] == (1,1,channel)
+	avg_pool = shared_layer_one(avg_pool)
+	assert avg_pool._keras_shape[1:] == (1,1,channel//ratio)
+	avg_pool = shared_layer_two(avg_pool)
+	assert avg_pool._keras_shape[1:] == (1,1,channel)
+	
+	max_pool = GlobalMaxPooling2D()(input_feature)
+	max_pool = Reshape((1,1,channel))(max_pool)
+	assert max_pool._keras_shape[1:] == (1,1,channel)
+	max_pool = shared_layer_one(max_pool)
+	assert max_pool._keras_shape[1:] == (1,1,channel//ratio)
+	max_pool = shared_layer_two(max_pool)
+	assert max_pool._keras_shape[1:] == (1,1,channel)
+	
+	cbam_feature = Add()([avg_pool,max_pool])
+	cbam_feature = Activation('hard_sigmoid')(cbam_feature)
+	
+	if K.image_data_format() == "channels_first":
+		cbam_feature = Permute((3, 1, 2))(cbam_feature)
+	
+	return multiply([input_feature, cbam_feature])
+
+
+def spatial_attention(input_feature):
+	kernel_size = 7
+	if K.image_data_format() == "channels_first":
+		channel = input_feature._keras_shape[1]
+		cbam_feature = Permute((2,3,1))(input_feature)
+	else:
+		channel = input_feature._keras_shape[-1]
+		cbam_feature = input_feature
+	
+	avg_pool = Lambda(lambda x: K.mean(x, axis=3, keepdims=True))(cbam_feature)
+	assert avg_pool._keras_shape[-1] == 1
+	max_pool = Lambda(lambda x: K.max(x, axis=3, keepdims=True))(cbam_feature)
+	assert max_pool._keras_shape[-1] == 1
+	concat = Concatenate(axis=3)([avg_pool, max_pool])
+	assert concat._keras_shape[-1] == 2
+	cbam_feature = Conv2D(filters = 1,
+					kernel_size=kernel_size,
+					activation = 'hard_sigmoid',
+					strides=1,
+					padding='same',
+					kernel_initializer='he_normal',
+					use_bias=False)(concat)
+	assert cbam_feature._keras_shape[-1] == 1
+	
+	if K.image_data_format() == "channels_first":
+		cbam_feature = Permute((3, 1, 2))(cbam_feature)
+		
+	return multiply([input_feature, cbam_feature])
+
+
+def cbam_block(cbam_feature,ratio=8):
+	cbam_feature = channel_attention(cbam_feature, ratio)
+	cbam_feature = spatial_attention(cbam_feature, )
+	return cbam_feature
+
+
+def cbam_mdl(inputs):
+    inputs_channels=int(inputs.shape[-1])
+    x=keras.layers.GlobalAveragePooling2D()(inputs)
+    x=keras.layers.Dense(int(inputs_channels/4))(x)
+    x=keras.layers.Activation('relu')(x)
+    x=keras.layers.Dense(int(inputs_channels))(x)
+    x=keras.layers.Activation('softmax')(x)
+    x=keras.layers.Reshape((1,1,inputs_channels))(x)
+    x=keras.layers.Multiply()([inputs,x])
+    return x
+
+
+class SeBlock(keras.layers.Layer):   
+    def __init__(self, reduction=4,**kwargs):
+        super(SeBlock,self).__init__(**kwargs)
+        self.reduction = reduction
+    def build(self,input_shape):#构建layer时需要实现
+    	#input_shape     
+    	pass
+    def call(self, inputs):
+        x = keras.layers.GlobalAveragePooling2D()(inputs)
+        x = keras.layers.Dense(int(x.shape[-1]) // self.reduction, use_bias=False,activation=keras.activations.relu)(x)
+        x = keras.layers.Dense(int(inputs.shape[-1]), use_bias=False,activation=keras.activations.hard_sigmoid)(x)
+        return keras.layers.Multiply()([inputs,x])    #weight channel
+        #return inputs*x 
+
+
+
+# 数据集
+train_dir='/home/cjd/38_Insect_pest/train_seg/'  # training set
+test_dir = '/home/cjd/38_Insect_pest/test_seg_pred/Pieris rapae Linnaeus'  # test set
+img_size = (224, 224)  # 图片大小
+epochs = 50
+MODEL_PATH = './obj_reco/tst_model.h5'
+board_name1 = './obj_reco/stage1/' + now + '/'
+board_name2 = './obj_reco/stage2/' + now + '/'
+now = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
+
+
+# 定义模型
+classes = sorted([o for o in os.listdir(train_dir)])  
+
+
+
+#----------Xnception-SE model---------------------------------------------------------------------
+from keras.applications.inception_resnet_v2 import InceptionResNetV2,preprocess_input
+from keras.applications.xception import Xception
+from keras.layers import Dense, GlobalAveragePooling2D,BatchNormalization,MaxPool2D
+from keras.models import Model
+import tensorflow as tf
+
+def conv_bn(x, filters, kernel_size, strides=1):    
+    x = Conv2D(filters=filters, 
+               kernel_size = kernel_size, 
+               strides=strides, 
+               padding = 'same', 
+               use_bias = False)(x)
+    x = BatchNormalization()(x)
+    return x
+
+def sep_bn(x, filters, kernel_size, strides=1):
+    
+    x = SeparableConv2D(filters=filters, 
+                        kernel_size = kernel_size, 
+                        strides=strides, 
+                        padding = 'same', 
+                        use_bias = False)(x)
+    x = BatchNormalization()(x)
+    return x
+
+
+base_model = Xception(weights='/home/cjd/01_rice_dete/obj_reco/checkpoint/xception_weights_tf_dim_ordering_tf_kernels_notop.h5', include_top=False)
+ 
+for layer in base_model.layers:
+    layer.trainable = False
+    
+x = base_model. layers[-7].output  
+tensor = base_model. layers[-14].output     
+x = sep_bn(x, filters = 1536,  kernel_size=5)
+x = ReLU()(x)
+x=SeBlock()(x)
+x = sep_bn(x, filters = 2048,  kernel_size=1)
+
+
+x = GlobalAveragePooling2D()(x) 
+predictions = Dense(len(classes), activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+#编译模型
+model.compile(optimizer='adam', loss='categorical_crossentropy',  metrics = ['accuracy'])  #rmsprop
+
+
+
+model.load_weights(MODEL_PATH)
+# --------------- Test ----------------
+dirs = os.listdir(test_dir)
+
+prob_list = []
+for d in dirs:
+    img = image.load_img(test_dir + os.sep + d, target_size=img_size)
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    y = model.predict(x)
+#    print(y)
+    print(classes[np.argmax(y)])
+#    print(classes)
+
+    prob_list.append(str(y))   
+    file=open('pred_prob.txt','w') 
+    file.write('\n'.join(prob_list))
+    file.close() 
---
+++ Insect_pest/pltV3_SE.py
@@ -0,0 +1,366 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 16 22:23:24 2019
+
+@author: HP
+"""
+
+
+
+from keras.layers import GlobalAveragePooling2D, GlobalMaxPooling2D, Reshape, Dense, multiply, Permute, Concatenate, Conv2D, Add, Activation, Lambda
+from keras import backend as K
+from keras.activations import sigmoid
+
+
+from keras import backend as K
+from keras import optimizers
+from keras.optimizers import Adam
+import keras.backend.tensorflow_backend as KTF
+import glob
+from keras.layers import Input,Dense,Dropout,BatchNormalization,Conv2D,MaxPooling2D,AveragePooling2D,concatenate,Activation,ZeroPadding2D
+import tensorflow as tf
+import cv2
+import numpy as np
+import pandas as pd
+import keras
+from keras.models import load_model
+from keras.layers import Activation, Dense
+from matplotlib import pyplot as plt
+from skimage import io,data
+import time
+from keras import layers
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.preprocessing.image import ImageDataGenerator
+
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+now = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
+
+from keras.layers import SeparableConv2D,ReLU
+
+import os,sys
+os.getcwd()
+os.chdir("/home/cjd/41_pest_pub")
+
+print(os.getcwd())
+print (sys.version)
+
+
+#import os
+# 
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "5,3"
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+keras.backend.tensorflow_backend.set_session(tf.Session(config=config))
+
+
+
+def replace_intermediate_layer_in_keras(model, layer_id, new_layer):
+    from keras.models import Model
+
+    layers = [l for l in model.layers]
+
+    x = layers[0].output
+    for i in range(1, len(layers)):
+        if i == layer_id:
+            x = new_layer(x)
+        else:
+            x = layers[i](x)
+
+    new_model = Model(input=layers[0].input, output=x)
+    return new_model
+
+def insert_intermediate_layer_in_keras(model, layer_id, new_layer):
+    from keras.models import Model
+
+    layers = [l for l in model.layers]
+
+    x = layers[0].output
+    for i in range(1, len(layers)):
+        if i == layer_id:
+            x = new_layer(x)
+        x = layers[i](x)
+
+    new_model = Model(input=layers[0].input, output=x)
+    return new_model
+
+
+
+
+import tensorflow as tf       
+def focal_loss(gamma=2.):            
+    def focal_loss_fixed(y_true, y_pred):
+        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+        return -K.sum( K.pow(1. - pt_1, gamma) * K.log(pt_1)) 
+    return focal_loss_fixed
+
+
+def Conv2d_BN(x, nb_filter,kernel_size, strides=(1,1), padding='same',name=None):  
+    if name is not None:  
+        bn_name = name + '_bn'  
+        conv_name = name + '_conv'  
+    else:  
+        bn_name = None  
+        conv_name = None  
+  
+    x = Conv2D(nb_filter,kernel_size,padding=padding,strides=strides,activation='relu',name=conv_name)(x)  
+    x = BatchNormalization(axis=3,name=bn_name)(x)  
+    return x  
+
+def Conv_Block(inpt,nb_filter,kernel_size,strides=(1,1), with_conv_shortcut=False):  
+    x = Conv2d_BN(inpt,nb_filter=nb_filter[0],kernel_size=(1,1),strides=strides,padding='same')  
+    x = Conv2d_BN(x, nb_filter=nb_filter[1], kernel_size=(3,3), padding='same')  
+    x = Conv2d_BN(x, nb_filter=nb_filter[2], kernel_size=(1,1), padding='same')  
+    if with_conv_shortcut:  
+        shortcut = Conv2d_BN(inpt,nb_filter=nb_filter[2],strides=strides,kernel_size=kernel_size)  
+        x = add([x,shortcut])  
+        return x  
+    else:  
+        x = add([x,inpt])  
+        return x  
+
+
+def channel_attention(input_feature, ratio=8):
+	
+	channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+	channel = input_feature._keras_shape[channel_axis]
+	
+	shared_layer_one = Dense(channel//ratio,
+							 kernel_initializer='he_normal',
+							 activation = 'relu',
+							 use_bias=True,
+							 bias_initializer='zeros')
+
+	shared_layer_two = Dense(channel,
+							 kernel_initializer='he_normal',
+							 use_bias=True,
+							 bias_initializer='zeros')
+	
+	avg_pool = GlobalAveragePooling2D()(input_feature)    
+	avg_pool = Reshape((1,1,channel))(avg_pool)
+	assert avg_pool._keras_shape[1:] == (1,1,channel)
+	avg_pool = shared_layer_one(avg_pool)
+	assert avg_pool._keras_shape[1:] == (1,1,channel//ratio)
+	avg_pool = shared_layer_two(avg_pool)
+	assert avg_pool._keras_shape[1:] == (1,1,channel)
+	
+	max_pool = GlobalMaxPooling2D()(input_feature)
+	max_pool = Reshape((1,1,channel))(max_pool)
+	assert max_pool._keras_shape[1:] == (1,1,channel)
+	max_pool = shared_layer_one(max_pool)
+	assert max_pool._keras_shape[1:] == (1,1,channel//ratio)
+	max_pool = shared_layer_two(max_pool)
+	assert max_pool._keras_shape[1:] == (1,1,channel)
+	
+	cbam_feature = Add()([avg_pool,max_pool])
+	cbam_feature = Activation('hard_sigmoid')(cbam_feature)
+	
+	if K.image_data_format() == "channels_first":
+		cbam_feature = Permute((3, 1, 2))(cbam_feature)
+	
+	return multiply([input_feature, cbam_feature])
+
+
+def spatial_attention(input_feature):
+	kernel_size = 7
+	if K.image_data_format() == "channels_first":
+		channel = input_feature._keras_shape[1]
+		cbam_feature = Permute((2,3,1))(input_feature)
+	else:
+		channel = input_feature._keras_shape[-1]
+		cbam_feature = input_feature
+	
+	avg_pool = Lambda(lambda x: K.mean(x, axis=3, keepdims=True))(cbam_feature)
+	assert avg_pool._keras_shape[-1] == 1
+	max_pool = Lambda(lambda x: K.max(x, axis=3, keepdims=True))(cbam_feature)
+	assert max_pool._keras_shape[-1] == 1
+	concat = Concatenate(axis=3)([avg_pool, max_pool])
+	assert concat._keras_shape[-1] == 2
+	cbam_feature = Conv2D(filters = 1,
+					kernel_size=kernel_size,
+					activation = 'hard_sigmoid',
+					strides=1,
+					padding='same',
+					kernel_initializer='he_normal',
+					use_bias=False)(concat)
+	assert cbam_feature._keras_shape[-1] == 1
+	
+	if K.image_data_format() == "channels_first":
+		cbam_feature = Permute((3, 1, 2))(cbam_feature)
+		
+	return multiply([input_feature, cbam_feature])
+
+
+def cbam_block(cbam_feature,ratio=8):
+	cbam_feature = channel_attention(cbam_feature, ratio)
+	cbam_feature = spatial_attention(cbam_feature, )
+	return cbam_feature
+
+
+def cbam_mdl(inputs):
+    inputs_channels=int(inputs.shape[-1])
+    x=keras.layers.GlobalAveragePooling2D()(inputs)
+    x=keras.layers.Dense(int(inputs_channels/4))(x)
+    x=keras.layers.Activation('relu')(x)
+    x=keras.layers.Dense(int(inputs_channels))(x)
+    x=keras.layers.Activation('softmax')(x)
+    x=keras.layers.Reshape((1,1,inputs_channels))(x)
+    x=keras.layers.Multiply()([inputs,x])
+    return x
+
+
+class SeBlock(keras.layers.Layer):   
+    def __init__(self, reduction=4,**kwargs):
+        super(SeBlock,self).__init__(**kwargs)
+        self.reduction = reduction
+    def build(self,input_shape):#构建layer时需要实现
+    	#input_shape     
+    	pass
+    def call(self, inputs):
+        x = keras.layers.GlobalAveragePooling2D()(inputs)
+        x = keras.layers.Dense(int(x.shape[-1]) // self.reduction, use_bias=False,activation=keras.activations.relu)(x)
+        x = keras.layers.Dense(int(inputs.shape[-1]), use_bias=False,activation=keras.activations.hard_sigmoid)(x)
+        return keras.layers.Multiply()([inputs,x])    #weighted channel
+        #return inputs*x 
+
+
+
+batch_size = 64 
+epochs = 30
+MODEL_INIT = './obj_reco/init_model.h5'
+MODEL_PATH = './obj_reco/tst_model.h5'
+board_name1 = './obj_reco/stage1/' + now + '/'
+board_name2 = './obj_reco/stage2/' + now + '/'
+train_dir='/home/cjd/38_Insect_pest/train_seg/'
+validation_dir='/home/cjd/38_Insect_pest/test_seg/'
+
+#train_dir='./train_seg/'
+#validation_dir='./test_seg/'
+
+#train_dir='/home/wkq/Projects/kerasVGG19/train_b/'
+#validation_dir='/home/wkq/Projects/kerasVGG19/test_b/'
+
+img_size = (224, 224)  # 图片大小
+#classes=list(range(1,5))
+#classes=['1','2','3','4']
+nb_train_samples = len(glob.glob(train_dir + '/*/*.*'))  
+nb_validation_samples = len(glob.glob(validation_dir + '/*/*.*'))  
+
+classes = sorted([o for o in os.listdir(train_dir)])  
+
+
+
+#----------Xnception SE model---------------------------------------------------------------------
+from keras.applications.inception_resnet_v2 import InceptionResNetV2,preprocess_input
+from keras.applications.xception import Xception
+from keras.layers import Dense, GlobalAveragePooling2D,BatchNormalization,MaxPool2D
+from keras.models import Model
+import tensorflow as tf
+
+def conv_bn(x, filters, kernel_size, strides=1):    
+    x = Conv2D(filters=filters, 
+               kernel_size = kernel_size, 
+               strides=strides, 
+               padding = 'same', 
+               use_bias = False)(x)
+    x = BatchNormalization()(x)
+    return x
+
+def sep_bn(x, filters, kernel_size, strides=1):
+    
+    x = SeparableConv2D(filters=filters, 
+                        kernel_size = kernel_size, 
+                        strides=strides, 
+                        padding = 'same', 
+                        use_bias = False)(x)
+    x = BatchNormalization()(x)
+    return x
+
+
+base_model = Xception(weights='/home/cjd/01_rice_dete/obj_reco/checkpoint/xception_weights_tf_dim_ordering_tf_kernels_notop.h5', include_top=False)
+ 
+for layer in base_model.layers:
+    layer.trainable = False
+    
+x = base_model. layers[-7].output  
+tensor = base_model. layers[-14].output     
+x = sep_bn(x, filters = 1536,  kernel_size=5)
+x = ReLU()(x)
+#x=SeBlock()(x)
+x = sep_bn(x, filters = 2048,  kernel_size=1)
+
+
+x = GlobalAveragePooling2D()(x) 
+predictions = Dense(len(classes), activation='softmax', name='visualized_layer')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+#edit model
+model.compile(optimizer='adam', loss='categorical_crossentropy',  metrics = ['accuracy'])  #rmsprop
+
+
+
+
+train_datagen = ImageDataGenerator(validation_split=0.2)
+train_datagen.mean = np.array([103.939, 116.779, 123.68], dtype=np.float32).reshape((3, 1, 1))  # 去掉imagenet BGR均值
+train_data = train_datagen.flow_from_directory(train_dir, target_size=img_size, classes=classes)
+validation_datagen = ImageDataGenerator()
+validation_datagen.mean = np.array([103.939, 116.779, 123.68], dtype=np.float32).reshape((3, 1, 1))
+validation_data = validation_datagen.flow_from_directory(validation_dir, target_size=img_size, classes=classes)
+
+
+#model_checkpoint1 = ModelCheckpoint(filepath=MODEL_INIT, save_best_only=True, monitor='val_accuracy', mode='max')
+model_checkpoint1 = ModelCheckpoint(filepath=MODEL_INIT, monitor='val_accuracy')
+board1 = TensorBoard(log_dir=board_name1,
+                     histogram_freq=0,
+                     write_graph=True,
+                     write_images=True)
+callback_list1 = [model_checkpoint1, board1]
+
+model.fit_generator(train_data, steps_per_epoch=nb_train_samples / float(batch_size),
+                           epochs = epochs-28,
+                           validation_steps=nb_validation_samples / float(batch_size),
+                           validation_data=validation_data,
+                           callbacks=callback_list1, verbose=2)
+
+#---------------2nd stage---------------------------------------------
+model_checkpoint2 = ModelCheckpoint(filepath=MODEL_PATH,  monitor='val_accuracy')
+board2 = TensorBoard(log_dir=board_name2,
+                     histogram_freq=0,
+                     write_graph=True,
+                     write_images=True)
+callback_list2 = [model_checkpoint2, board2]
+
+
+model.load_weights(MODEL_INIT)
+for model1 in model.layers:
+    model1.trainable = True
+#fine_tune_at = 50
+#for layer in model.layers[:fine_tune_at]:
+#    layer.trainable = False
+
+
+#model.compile(optimizer='adam', loss =[focal_loss(gamma=2)], metrics=['accuracy'])
+#model.compile(optimizer=optimizers.Adam(), loss =[focal_loss(gamma=2)], metrics=['accuracy']) #loss='categorical_crossentropy',
+model.compile(optimizer=optimizers.SGD(lr=0.001), loss = [focal_loss(gamma=2)], metrics=['accuracy']) #loss='categorical_crossentropy',
+#model.compile(optimizer=optimizers.Adadelta(), loss = [focal_loss(gamma=2)], metrics=['accuracy']) #loss='categorical_crossentropy',
+
+model.fit_generator(train_data, steps_per_epoch=nb_train_samples / float(batch_size), epochs=epochs,
+                    validation_data=validation_data, validation_steps=nb_validation_samples / float(batch_size),
+                    callbacks=callback_list2, verbose=2)
+
+
+
+'''
+#model.summary()  output
+from contextlib import redirect_stdout   
+with open('model_summary.txt', 'w') as f:
+    with redirect_stdout(f):
+        model.summary(line_length=200,positions=[0.30,0.60,0.7,1.0])
+
+from keras.utils import plot_model
+plot_model(model,to_file="model.png",show_shapes=True)
+'''
+
+
+
